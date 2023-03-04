38ad59e3fa090e5d0b0d6d66480fc61f59d1d253
salmankarim42 <salmankarim.khan42@gmail.com>
2023-03-05 00:02:45 +0500

    working on bottom tab navigation

--- src/Config/Screens/BottomTabScreens/bottomTabNav.js
+++ src/Config/Screens/BottomTabScreens/bottomTabNav.js
@@ -1,12 +1,13 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { View, StyleSheet } from 'react-native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {View, StyleSheet} from 'react-native';
 
 import Favorites from './favorite';
 import ShoppingCart from './shoppingCart';
 import Profile from './profile';
 import Home from '../home';
-import { Icon } from '../../../Utils/icons/Icon';
-import { cusColors } from '../../../Utils/colors';
+import {Icon} from '../../../Utils/icons/Icon';
+import {cusColors} from '../../../Utils/colors';
+import {TabView} from 'react-native-tab-view';
 
 const BotTab = createMaterialBottomTabNavigator();
 
@@ -14,27 +15,52 @@ export default function BotTabNav() {
   return (
     <View style={styles.mainView}>
       <BotTab.Navigator
+        labeled={false}
         barStyle={{
           height: 65,
           position: 'absolute',
           overflow: 'hidden',
           borderTopLeftRadius: 30,
           borderTopRightRadius: 30,
-          backgroundColor: cusColors.themeColorDark,
+          backgroundColor: 'green',
         }}
-      >
+        screenOptions={{tabBarColor: 'black'}}>
         <BotTab.Screen
           name="Home"
           component={Home}
           options={{
             tabBarIcon: () => (
-              <Icon type='fontAwesome' name='home' size={25} color='black' />
-            )
+              <Icon type="fontAwesome" name="home" size={20} color="black" />
+            ),
           }}
         />
-        <BotTab.Screen name="Favorites" component={Favorites} />
-        <BotTab.Screen name="ShoppingCart" component={ShoppingCart} />
-        <BotTab.Screen name="Profile" component={Profile} />
+        <BotTab.Screen
+          name="Favorites"
+          component={Favorites}
+          options={{
+            tabBarIcon: () => (
+              <Icon type="fontAwesome" name="home" size={20} color="black" />
+            ),
+          }}
+        />
+        <BotTab.Screen
+          name="ShoppingCart"
+          component={ShoppingCart}
+          options={{
+            tabBarIcon: () => (
+              <Icon type="fontAwesome" name="home" size={20} color="black" />
+            ),
+          }}
+        />
+        <BotTab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: () => (
+              <Icon type="fontAwesome" name="home" size={20} color="black" />
+            ),
+          }}
+        />
       </BotTab.Navigator>
     </View>
   );
